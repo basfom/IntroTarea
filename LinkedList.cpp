a2368149c99a965d9aa5030fc26c31ede5202be3
Gaspar Octavio <gasparcorreavergara@gmail.com>
2016-10-28 10:28:01 -0300

    Se añade funcion add_node()

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -8,9 +8,22 @@ struct node
     node *next;
 };
 
+
+void add_node( node *ptr, int value  )
+{
+    node *new_node = new node;
+    new_node -> value =  value;
+    new_node -> next = (ptr -> next);
+    ptr -> next = new_node;
+}
+
 int main()
 {
     node *ptr;
-    ptr = NULL;
+    ptr = new node;
+    ptr -> value = 1;
+    ptr -> next = NULL;
+    add_node(ptr, 2);
+    cout << ptr->value << ptr->next->value<<endl;
     return 0;
 }
